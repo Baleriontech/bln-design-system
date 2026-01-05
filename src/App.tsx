8654b36297c6aa947a3e7a5d0f04412f1d9649ab
Krit <tanakrit@belerion.co.th>
2026-01-05 18:00:36 +0700

    ✨ Add StoryBook & Init Read Me
    issue:tailwind safeflight

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col gap-y-8'>
-        <h2>Bln-Design-System: PlayGround</h2>
-        <div className=' p-24 rounded-xl bg-gray-50 '>
-          <Button className='bg-white'>Test Import</Button>
+      <div className='flex flex-col items-center gap-y-8 bg-white'>
+        <h2 className='text-center'>Bln-Design-System: PlayGround</h2>
+        <div className='p-24 rounded-xl bg-gray-50 '>
+          <Button className='text-2xl'>Test</Button>
         </div>
       </div>
     </>
