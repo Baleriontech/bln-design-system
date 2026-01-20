546bce207b7857ab209c356abbb4b4909ec1065c
Krit <tanakrit@belerion.co.th>
2026-01-20 15:57:35 +0700

    📦️ New registry

---
+++ packages/button/src/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button'
+
+
+export * from "./Button"
+export type * from "./Button.types"
+
+export default Button;
--- packages/main.ts
+++
@@ -1,4 +0,0 @@
-import Button from "./button/src/Button"
-export { Button } 
-
-export type { IButtonProps } from "./button/src/Button.types"
