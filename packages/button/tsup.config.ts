7f8edcdc42cd92eab4a95a93c7fb6e05cf205c07
Krit <tanakrit@belerion.co.th>
2026-01-21 15:58:07 +0700

    🎨 Native css instead tailwind

--- packages/button/tsup.config.ts
+++ packages/button/tsup.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "tsup"
-import fs from "fs";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
@@ -9,7 +8,4 @@ export default defineConfig({
   sourcemap: true,
   external: ["react", "react-dom"],
   tsconfig: "./tsconfig.json",
-  onSuccess: async () => {
-    return await fs.copyFileSync("src/style.css", "dist/style.css");
-  }
 })
